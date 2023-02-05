c8ce334062ba2e1562e5b9a2c07f2302673e7873
Karl Hiner <karl.hiner@gmail.com>
2023-02-05 15:18:41 -0500

    Support `n % p != 0`

--- int_calc.cpp
+++ int_calc.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <math.h>
 #include <mpi.h>
@@ -20,9 +21,13 @@ int main(int argc, char* argv[]) {
     const int global_n = strtol(argv[1], nullptr, 0);
     const double h = 1.0 / double(global_n);
 
-    const int local_n = global_n / world_size; // Local problem size (assumes `N % world_size == 0`)
+    // Handle `n % p != 0` by distributing any extra `n % p` items evenly across the first `n % p` processors.
+    // In other words, each of the first `p - 1` processors may sum at most one additional entry.
+    const int n_remainder = global_n % world_size;
+    const int start_i = world_rank * (global_n / world_size) + 1 + (std::min(n_remainder, world_rank));
+    const int local_n = global_n / world_size + (n_remainder > 0 && world_rank < n_remainder ? 1 : 0);
     double local_sum = 0.0;
-    for (int i = world_rank * local_n + 1; i <= (world_rank + 1) * local_n; i++) {
+    for (int i = start_i; i < start_i + local_n; i++) {
         local_sum += 4.0/(1.0 + pow(h*(i - 0.5), 2));
     }
 
